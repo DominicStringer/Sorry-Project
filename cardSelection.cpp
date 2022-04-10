33923fb271c5e4e5696a0e2c516b8577f379be9d
RAJOTTEC <77853991+RAJOTTEC@users.noreply.github.com>
2022-04-10 15:37:59 -0700

    Add files via upload

--- cardSelection.cpp
+++ cardSelection.cpp
@@ -24,31 +24,37 @@ int cardSelection()
     
     if(num >= 1 && num <= 5)
     {
+        cout << "You drew a 1" << endl;
         return 1;
     }
     
     else if(num >= 6 && num <= 9)
     {
+        cout << "You drew a 2" << endl;
         return 2;
     }
     
     else if(num >= 10 && num <= 13)
     {
+        cout << "You drew a 3" << endl;
         return 3;
     }
     
     else if(num >= 14 && num <= 17)
     {
+        cout << "You drew a 4" << endl;
         return -4;
     }
     
     else if(num >= 18 && num <= 21)
     {
+        cout << "You drew a 5" << endl;
         return 5;
     }
     
     else if(num >= 22 && num <= 25)
     {
+        cout << "You drew a 7" << endl;
         do
         {
             cout << "Do you want to move one pawn forward 7 tiles(1) or two pawns forward seven combined tiles(2)? ";
@@ -66,11 +72,13 @@ int cardSelection()
     
     else if(num >= 26 && num <= 29)
     {
+        cout << "You drew an 8" << endl;
         return 8;
     }
     
     else if(num >= 30 && num <= 33)
     {
+        cout << "You drew a 10" << endl;
         do
         {
             cout << "do you want to move your pawn forward 10 tiles(1) or back 1 tile(2)? ";
@@ -88,16 +96,19 @@ int cardSelection()
     
     else if(num >= 34 && num <= 37)
     {
+        cout << "You drew an 11" << endl;
         return 11;
     }
     
     else if(num >= 38 && num <= 41)
     {
+        cout << "You drew a 12" << endl;
         return 12;
     }
     
     else if(num >= 42 && num <= 45)
     {
+        cout << "You drew a Sorry!" << endl;
         return 14;
     }
     
